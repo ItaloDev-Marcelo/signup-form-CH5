03eb6ff67fe55606c96e5f0e4341596be78ebd2c
Italo Marcelo <devfrontend2028study@gmail.com>
2025-01-15 17:27:13 -0300

    up SignUp test

--- src/components/form/SignUp.jsx
+++ src/components/form/SignUp.jsx
@@ -12,6 +12,8 @@ export default function SignUpForm() {
     password: ''
    })
 
+   const [error, setError] = useState(false)
+
    function ErrorTest() {
      const {firstName, lastName, email, password} = SignUpFormData;
 
@@ -20,32 +22,56 @@ export default function SignUpForm() {
      let img3 = document.querySelector('.error-sms3-img');
      let img4 = document.querySelector('.error-sms4-img');
 
+     let sms1 = document.querySelector('.error-sms1-txt'); 
+     let sms2 = document.querySelector('.error-sms2-txt');
+     let sms3 = document.querySelector('.error-sms3-txt');
+     let sms4 = document.querySelector('.error-sms4-txt');
 
 
-     if(firstName === '') {
-      img1.style.display = 'block'
-     }else {
-      img1.style.display = 'none'
-     }
+    
+      if(firstName === '') {
+        img1.style.display = 'block'
+         sms1.style.display = 'block'
+       }else {
+        img1.style.display = 'none'
+           sms1.style.display = 'none'
+       }
+  
+       if(lastName === ''){
+        img2.style.display = 'block'
+        sms2.style.display = 'block'
+       }else {
+        img2.style.display = 'none'
+        sms2.style.display = 'none'
+       }
+  
 
-     if(lastName === '' ){
-      img2.style.display = 'block'
-     }else {
-      img2.style.display = 'none'
-     }
+       const exp = /@gmail.com$|@hotmail.com$|@outlook.com$/
+  
+       if(email === ''){
+        img3.style.display = 'block'
+        sms3.style.display = 'block'
+       }else if(!exp.test(email)) {
+        img3.style.display = 'block'
+        sms3.style.display = 'block'
+       }else {
+        
+        img3.style.display = 'none'
+        sms3.style.display = 'none'
+       }
+  
+       if(password === ''){
+        img4.style.display = 'block'
+        sms4.style.display = 'block'
+       }else {
+        img4.style.display = 'none'
+        sms4.style.display = 'none'
+       }
+    
 
-
-     if(email === '' ){
-      img3.style.display = 'block'
-     }else {
-      img3.style.display = 'none'
-     }
-
-     if(password === '' ){
-      img4.style.display = 'block'
-     }else {
-      img4.style.display = 'none'
-     }
+       console.log(exp.test(email))
+    
+     
 
    }
  
@@ -59,13 +85,11 @@ export default function SignUpForm() {
   }
   
    function hundleSignUpSubmit(event) {
-     event.preventDefault()  
-     useEffect(() => {
-       ErrorTest()
-     })
-   }
+     event.preventDefault()    
+     setError(true)   
+     error ? ErrorTest() : null
 
-
+  }
 
     return (
 
@@ -79,32 +103,32 @@ export default function SignUpForm() {
 <SignUpInput name={'firstName'}  id={'first Name'} type={'text'} 
  value={SignUpFormData.firstName} onChange={hundleChange} 
   />
- <img  className='error-sms1-img' src={ErrorIconSvg}/> 
- <p className='error-sms1-txt'>First name cannot be empty </p>
+  {error ?<><img  className='error-sms1-img' src={ErrorIconSvg}/> 
+  <p className='error-sms1-txt'>First name cannot be empty </p></> : <></>}
 </div>
 
 <div className="Row-item">
 <SignUpInput name={'lastName'}    id={'last Name'} type={'text'} 
  value={SignUpFormData.lastName} onChange={hundleChange} 
  />
-<img  className='error-sms2-img' src={ErrorIconSvg}/>
-<p className='error-sms2-txt'>Last name cannot be empty </p>
+   {error ? <><img  className='error-sms2-img' src={ErrorIconSvg}/> 
+   <p className='error-sms2-txt'>Last name cannot be empty </p></> : null}
 </div>
 
 <div className="Row-item">
 <SignUpInput name={'email'}   id={'email'} type={'email'} 
  value={SignUpFormData.email} onChange={hundleChange} 
 />
-<img  className='error-sms3-img' src={ErrorIconSvg}/>
-<p className='error-sms3-txt'>email cannot be empty </p>
+{error ?<><img  className='error-sms3-img' src={ErrorIconSvg}/> 
+<p className='error-sms3-txt'>Email cannot be empty </p></> : null}
 </div>
 
 <div className="Row-item">
 <SignUpInput name={'password'}  id={'password'} type={'password'} 
  value={SignUpFormData.password} onChange={hundleChange} 
   />
-    <img  className='error-sms4-img' src={ErrorIconSvg}/> 
-  <p className='error-sms4-txt'>password cannot be empty </p>
+     {error ?<><img  className='error-sms4-img' src={ErrorIconSvg}/> 
+     <p className='error-sms4-txt'>password cannot be empty </p></> : null}
 </div>
 
 <button type="submit" id="submit-btn"> Claim your free trial </button>
