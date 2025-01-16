548d842584bb12b2c1baaaa1e520d041597e11b0
Italo Marcelo <devfrontend2028study@gmail.com>
2025-01-16 16:25:48 -0300

    up

--- src/components/form/SignUp.jsx
+++ src/components/form/SignUp.jsx
@@ -27,38 +27,56 @@ export default function SignUpForm() {
     let sms3 = document.querySelector('.error-sms3-txt');
     let sms4 = document.querySelector('.error-sms4-txt');
 
-
+    let inputstyle1 = document.getElementById('first--Name');
+    let inputstyle2 = document.getElementById('last--Name');
+    let inputstyle3 = document.getElementById('email');
+    let inputstyle4 = document.getElementById('password');
 
     if (firstName === '') {
+      
+      inputstyle1.style.borderColor = 'red'
       img1.style.display = 'block'
       sms1.style.display = 'block'
     } else {
+      
+      inputstyle1.style.borderColor = '#b9b6d3'
       img1.style.display = 'none'
       sms1.style.display = 'none'
     }
 
     if (lastName === '') {
+      
+      inputstyle2.style.borderColor = 'red'
       img2.style.display = 'block'
       sms2.style.display = 'block'
     } else {
+      
+      inputstyle2.style.borderColor = '#b9b6d3'
       img2.style.display = 'none'
       sms2.style.display = 'none'
     }
 
 
     if (email === '') {
+      
+      inputstyle3.style.borderColor = 'red'
       img3.style.display = 'block'
       sms3.style.display = 'block'
     } else {
-
+      
+      inputstyle3.style.borderColor = '#b9b6d3'
       img3.style.display = 'none'
       sms3.style.display = 'none'
     }
 
     if (password === '') {
+      
+      inputstyle4.style.borderColor = 'red'
       img4.style.display = 'block'
       sms4.style.display = 'block'
     } else {
+      
+      inputstyle4.style.borderColor = '#b9b6d3'
       img4.style.display = 'none'
       sms4.style.display = 'none'
     }
@@ -93,7 +111,7 @@ export default function SignUpForm() {
       <form onSubmit={hundleSignUpSubmit} id='form--container'>
 
         <div className="Row-item">
-          <SignUpInput name={'firstName'} id={'first Name'} type={'text'}
+          <SignUpInput name={'firstName'} id={'first--Name'} type={'text'}
             value={SignUpFormData.firstName} onChange={hundleChange}
           />
           {error ? <><img className='error-sms1-img' src={ErrorIconSvg} />
@@ -101,7 +119,7 @@ export default function SignUpForm() {
         </div>
 
         <div className="Row-item">
-          <SignUpInput name={'lastName'} id={'last Name'} type={'text'}
+          <SignUpInput name={'lastName'} id={'last--Name'} type={'text'}
             value={SignUpFormData.lastName} onChange={hundleChange}
           />
           {error ? <><img className='error-sms2-img' src={ErrorIconSvg} />
